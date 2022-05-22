3cc05ce1b22402a1bbc3d51529f25040961616e0
ATAMATODE <atamatode@gmail.com>
2022-05-22 17:54:25 -0500

    space allocation in the code

--- Color-FLippers/setup/rgba color/rgba.js
+++ Color-FLippers/setup/rgba color/rgba.js
@@ -1,27 +1,6 @@
-/*const rgba = {red: "rgba(255,0,0,0.3)",
-              green: "rgba(0,255,0,0.3)",
-              blue : "rgba(0,0,255,0.3)",
-              opc: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-              }
-              //targeting the first index
-for(i= 0; i<256; i++){document.write(i);}
-// target the button
-const btn = document.getElementById("btn");
-// target the color text element
-const color = document.querySelector(".color");
 
-// Callback Function
-function getRandomNumber() {
-    return Math.floor(Math.random() *256)
-}
-function randomOpc(){for(let i=0; i< 1; i++)
-    {return Math.floor(Math.random(i));}}
-
-    function randomNumber() { for(let i= 0; i < 256; i++){
-        return Math.floor(Math.random(i)*256);
-        }}
 // create and event listener for the button and the background.
-btn.addEventListener("click", function () { // Higher order function
+/*btn.addEventListener("click", function () { // Higher order function
     let rgbaColor = `rgba(${randomNumber},${randomNumber},${randomNumber},${randomOpc})`;
     for (let i = 0; i <= 4; i++) {
         rgbaColor += getRandomNumber();
